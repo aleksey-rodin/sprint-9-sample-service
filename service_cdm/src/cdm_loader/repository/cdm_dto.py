17d7d04414071d85475257b0820e80195683182f
Aleksey Rodin <rodin.aleksey.sergeevich@gmail.com>
2025-06-10 18:10:58 +0300

    Fix files

--- service_cdm/src/cdm_loader/repository/cdm_dto.py
+++ service_cdm/src/cdm_loader/repository/cdm_dto.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from uuid import UUID
 
 
-class ProductCounters(BaseModel):
+class ProductCountersDTO(BaseModel):
     user_id: UUID
     product_id: UUID
     product_name: str
